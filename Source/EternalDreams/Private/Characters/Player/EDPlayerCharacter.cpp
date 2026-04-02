da4ba635c7dcd7ffd15668d5c79b85e2320c4d9b
khs081215 <39721769+khs081215@users.noreply.github.com>
2026-04-02 19:07:06 +0900

    feat: Add BaseAttributeSet
    1) 어트리뷰트셋 중 몬스터와 중복되는 부분인 Base를 제작하였습니다.
    2) 캐릭터에 ASC 컴포넌트와 카메라 컴포넌트를 추가하였습니다.

---
+++ Source/EternalDreams/Private/Characters/Player/EDPlayerCharacter.cpp
@@ -0,0 +1,74 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/Player/EDPlayerCharacter.h"
+
+#include "AbilitySystemComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Characters/Base/GAS/EDBaseAttributeSet.h"
+#include "Characters/Player/GAS/EDPlayerAttributeSet.h"
+#include "GameFramework/SpringArmComponent.h"
+
+
+// Sets default values
+AEDPlayerCharacter::AEDPlayerCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	//Camera 관련 생성
+	SpringArm=CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->TargetArmLength = SpringArmLength;
+	SpringArm->SetupAttachment(RootComponent);
+	Camera=CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+	
+	// ASC 생성
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+	
+}
+
+// Called when the game starts or when spawned
+void AEDPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	if (BPBaseAttributeSet)
+	{
+		BaseAttributeSet=NewObject<UEDBaseAttributeSet>();
+	}
+	if (BPPlayerAttributeSet)
+	{
+		PlayerAttributeSet=NewObject<UEDPlayerAttributeSet>();
+	}
+	
+	InitializeAbilitySystem();
+
+}
+
+// Called every frame
+void AEDPlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AEDPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+UAbilitySystemComponent* AEDPlayerCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AEDPlayerCharacter::InitializeAbilitySystem()
+{
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	}
+}
+
---
+++ Source/EternalDreams/Public/Characters/Player/EDPlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "EDPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ETERNALDREAMS_API AEDPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
