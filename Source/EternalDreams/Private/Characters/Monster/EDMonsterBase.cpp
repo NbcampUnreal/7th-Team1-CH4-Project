45661ab8aaba5a1016831a70fe55b60d3e7ef3ef
Yoongisang <sawsd@naver.com>
2026-04-02 20:48:04 +0900

    feat: GAS 및 리플리케이션 기반 몬스터 베이스 구현
    - IAbilitySystemInterface 상속 및 ASC 컴포넌트 추가
    - MonsterState ReplicatedUsing으로 서버-클라이언트 동기화
    - InitializeFromDataAsset으로 DataAsset 기반 초기화 지원
    - EDMonsterDataAsset getter 정비 및 MonsterClass 프로퍼티 추가

--- Source/EternalDreams/Private/Characters/Monster/EDMonsterBase.cpp
+++ Source/EternalDreams/Private/Characters/Monster/EDMonsterBase.cpp
@@ -2,13 +2,23 @@
 
 
 #include "Characters/Monster/EDMonsterBase.h"
-
+#include "AbilitySystemComponent.h"
+#include "Data/EDMonsterDataAsset.h"
+#include "Net/UnrealNetwork.h"
 
 // Sets default values
 AEDMonsterBase::AEDMonsterBase()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	// 몬스터 GE는 서버만 가지고있고 계산하기 때문에 Minimal로 설정
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+	// Idle상태로 시작
+	MonsterState = EMonsterState::Idle;
 }
 
 // Called when the game starts or when spawned
@@ -16,17 +26,34 @@ void AEDMonsterBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (IsValid(AbilitySystemComponent) == false)
+		return;
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
 }
 
-// Called every frame
-void AEDMonsterBase::Tick(float DeltaTime)
+void AEDMonsterBase::InitializeFromDataAsset(UEDMonsterDataAsset* InDataAsset)
 {
-	Super::Tick(DeltaTime);
+	if (InDataAsset == nullptr)
+		return;
+	DataAsset = InDataAsset;
+	
+	const FMonsterStatRow& Stat = InDataAsset->GetStat();
+	UE_LOG(LogTemp, Warning, TEXT("[%s] InitializeFromDataAsset - MaxHP: %.1f, Atk: %.1f"),
+		*GetName(), Stat.MaxHP, Stat.Atk)
+	// TODO : AttributeSet 연결 후 Stat적용 
+	// TODO : Mesh, AnimInstance, BT - DataAsset에 getter 추가 후 비동기 로드
 }
 
-// Called to bind functionality to input
-void AEDMonsterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AEDMonsterBase::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AEDMonsterBase, MonsterState);
 }
+
+void AEDMonsterBase::OnRep_MonsterState()
+{
+	UE_LOG(LogTemp, Warning, TEXT("[%s] Monster State: %d"), *GetName(), (int32)MonsterState);
+	// TODO: AnimInstance에 전달
+}
+
 
