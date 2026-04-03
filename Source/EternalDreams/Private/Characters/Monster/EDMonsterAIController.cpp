35392360ee0688a5da04083826dcb17db3d71de1
Yoongisang <sawsd@naver.com>
2026-04-03 22:26:06 +0900

    feat: MonsterAIController Perception 및 BT 로직 구현
    AIPerceptionComponent에 시각, 피격, 접촉, 청각, 팀 감각 설정.
    팀 판별(GenericTeamId) 구현, OnPossess에서 DataAsset으로부터
    BehaviorTree 로드, Perception 콜백에서 Blackboard 타겟/청각 상태
    업데이트 및 주변 아군에게 팀 자극 이벤트 전파 구현.

--- Source/EternalDreams/Private/Characters/Monster/EDMonsterAIController.cpp
+++ Source/EternalDreams/Private/Characters/Monster/EDMonsterAIController.cpp
@@ -2,12 +2,65 @@
 
 
 #include "Characters/Monster/EDMonsterAIController.h"
+#include "Characters/Monster/EDMonsterBase.h"
+#include "Data/EDMonsterDataAsset.h"
+#include "Perception/AIPerceptionSystem.h"
+#include "Perception/AISense_Sight.h"
+#include "Perception/AISense_Damage.h"
+#include "Perception/AISense_Touch.h"
+#include "Perception/AISense_Hearing.h"
+#include "Perception/AISense_Team.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 // Sets default values
 AEDMonsterAIController::AEDMonsterAIController()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	
+	// Perception
+	AIPerceptionComp = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerceptionComp"));
+	SetPerceptionComponent(*AIPerceptionComp);
+	// magic number는 추후에 수정 
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
+	SightConfig->SightRadius = 1000.f;
+	SightConfig->LoseSightRadius = 1200.f;
+	SightConfig->PeripheralVisionAngleDegrees = 60.f;
+	SightConfig->SetMaxAge(5.f);
+	SightConfig->DetectionByAffiliation.bDetectEnemies= true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = false;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = false;
+	
+	DamageConfig = CreateDefaultSubobject<UAISenseConfig_Damage>(TEXT("DamageConfig"));
+	DamageConfig->SetMaxAge(5.f);
+	
+	TouchConfig = CreateDefaultSubobject<UAISenseConfig_Touch>(TEXT("TouchConfig"));
+	TouchConfig->SetMaxAge(5.f);
+	TouchConfig->DetectionByAffiliation.bDetectEnemies = true;
+	TouchConfig->DetectionByAffiliation.bDetectFriendlies = false;
+	TouchConfig->DetectionByAffiliation.bDetectNeutrals = false;
+	
+	HearingConfig = CreateDefaultSubobject<UAISenseConfig_Hearing>(TEXT("HearingConfig"));
+	HearingConfig->HearingRange = 800.f;
+	HearingConfig->SetMaxAge(5.f);
+	HearingConfig->DetectionByAffiliation.bDetectEnemies = true;
+	HearingConfig->DetectionByAffiliation.bDetectFriendlies = false;
+	HearingConfig->DetectionByAffiliation.bDetectNeutrals = false;
+	
+	TeamConfig = CreateDefaultSubobject<UAISenseConfig_Team>(TEXT("TeamConfig"));
+	TeamConfig->SetMaxAge(5.f);
+	
+	AIPerceptionComp->ConfigureSense(*SightConfig);
+	AIPerceptionComp->ConfigureSense(*DamageConfig);
+	AIPerceptionComp->ConfigureSense(*TouchConfig);
+	AIPerceptionComp->ConfigureSense(*HearingConfig);
+	AIPerceptionComp->ConfigureSense(*TeamConfig);
+	AIPerceptionComp->SetDominantSense(SightConfig->GetSenseImplementation());
+	AIPerceptionComp->OnPerceptionUpdated.AddDynamic(this, &AEDMonsterAIController::OnPerceptionUpdated);
+	AIPerceptionComp->OnTargetPerceptionForgotten.AddDynamic(this, &AEDMonsterAIController::OnPerceptionForgotten);
+	
+	MonsterTeamId = FGenericTeamId(1);
 }
 
 // Called when the game starts or when spawned
@@ -19,25 +72,131 @@ void AEDMonsterAIController::BeginPlay()
 
 ETeamAttitude::Type AEDMonsterAIController::GetTeamAttitudeTowards(const AActor& Other) const
 {
-	return Super::GetTeamAttitudeTowards(Other);
+	const APawn* OtherPawn = Cast<APawn>(&Other);
+	if (IsValid(OtherPawn) == false)
+		return ETeamAttitude::Neutral;
+	
+	const IGenericTeamAgentInterface* OtherTeam = Cast<IGenericTeamAgentInterface>(OtherPawn->GetController());
+	if (OtherTeam == nullptr)
+		return ETeamAttitude::Neutral;
+	
+	return MonsterTeamId == OtherTeam->GetGenericTeamId() 
+	? ETeamAttitude::Friendly 
+	: ETeamAttitude::Hostile;
 }
 
 void AEDMonsterAIController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
+	
+	if (HasAuthority() == false)
+		return;
+	SetGenericTeamId(MonsterTeamId);
+	UAIPerceptionSystem::GetCurrent(GetWorld())->UpdateListener(*AIPerceptionComp);
+	
+	AEDMonsterBase* Monster = Cast<AEDMonsterBase>(InPawn);
+	if (IsValid(Monster) == false)
+		return;
+	
+	UEDMonsterDataAsset* DA = Monster->GetDataAsset();
+	if (IsValid(DA) == false)
+		return;
+	
+	UBehaviorTree* BT = DA->GetBehaviorTree().LoadSynchronous();
+	if (IsValid(BT) == false)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[%s] Failed to load BehaviorTree!"), *GetName());
+		return;
+	}
+	
+	RunBehaviorTree(BT);
 }
 
 void AEDMonsterAIController::OnUnPossess()
 {
 	Super::OnUnPossess();
+	GetWorldTimerManager().ClearTimer(TeamReportTimerHandle);
 }
 
 void AEDMonsterAIController::OnPerceptionUpdated(const TArray<AActor*>& UpdatedActors)
 {
+	if (HasAuthority() == false)
+		return;
+	
+	for (AActor* Actor : UpdatedActors)
+	{
+		if (IsValid(Actor) == false)
+			continue;
+		FActorPerceptionBlueprintInfo Info;
+		AIPerceptionComp->GetActorsPerception(Actor, Info);
+		
+		for (const FAIStimulus& Stimulus : Info.LastSensedStimuli)
+		{
+			if (Stimulus.WasSuccessfullySensed() == false)
+				continue;
+			TSubclassOf<UAISense> SenseClass = UAIPerceptionSystem::GetSenseClassForStimulus(GetWorld(), Stimulus);
+			if (IsValid(SenseClass) == false)
+				continue;
+			if (SenseClass == UAISense_Sight::StaticClass() ||
+				SenseClass == UAISense_Damage::StaticClass() ||
+				SenseClass == UAISense_Touch::StaticClass())
+			{
+				UE_LOG(LogTemp, Warning, TEXT("[%s] 감지: %s (%s)"), *GetName(), *Actor->GetName(), *SenseClass->GetName());
+				// TODO: BB_Monster 생성 후 활성화
+				UBlackboardComponent* BB = GetBlackboardComponent();
+				if (IsValid(BB) == false)
+					continue;
+				BB->SetValueAsObject(TEXT("TargetActor"), Actor);
+				// Team Sense로 주변 아군에게 타겟 공유
+				GetWorld()->GetTimerManager().SetTimer(TeamReportTimerHandle,
+					[this, Actor]()
+					{
+						if (IsValid(this) == false || IsValid(GetWorld()) == false || IsValid(Actor) == false)
+							return;
+						
+						UAIPerceptionSystem* PerceptionSystem = UAIPerceptionSystem::GetCurrent(GetWorld());
+						if (IsValid(PerceptionSystem) == false)
+							return;
+						FAITeamStimulusEvent Event = FAITeamStimulusEvent(this, Actor, Actor->GetActorLocation(), 1000.f);
+						PerceptionSystem->OnEvent(Event);
+					},
+					2.f, true, 0.5f);
+			}
+			// Hearing 감지
+			else if (SenseClass == UAISense_Hearing::StaticClass())
+			{
+				UE_LOG(LogTemp, Warning, TEXT("[%s] Hearing: %s 소리 감지"), *GetName(), *Actor->GetName());
+				// TODO: BB_Monster 생성 후 활성화
+				UBlackboardComponent* BB = GetBlackboardComponent();
+				if (IsValid(BB) == false)
+					continue;
+				BB->SetValueAsVector(TEXT("LastHearingLocation"), Actor->GetActorLocation());
+				BB->SetValueAsBool(TEXT("bIsTracking"), true);
+			}
+			// Team -> 아군으로부터 카겟 수신
+			else if (SenseClass == UAISense_Team::StaticClass())
+			{
+				UE_LOG(LogTemp, Warning, TEXT("[%s] Team: %s 정보 수신"), *GetName(), *Actor->GetName());
+				UBlackboardComponent* BB = GetBlackboardComponent();
+				if (IsValid(BB) == false)
+					continue;
+				if (IsValid(BB->GetValueAsObject(TEXT("TargetActor"))) == false)
+					BB->SetValueAsObject(TEXT("TargetActor"), Actor);
+			}
+		}
+	}
 }
 
 void AEDMonsterAIController::OnPerceptionForgotten(AActor* Actor)
 {
+	if (IsValid(Actor) == false)
+		return;
+	UE_LOG(LogTemp, Warning, TEXT("[%s] 잊혀진 액터: %s"), *GetName(),*Actor->GetName());
+	UBlackboardComponent* BB = GetBlackboardComponent();
+	if (IsValid(BB) == false)
+		return;
+	BB->SetValueAsVector(TEXT("LastHearingLocation"), FVector::ZeroVector);
+	BB->SetValueAsBool(TEXT("bIsTracking"), false);
 }
 
 
