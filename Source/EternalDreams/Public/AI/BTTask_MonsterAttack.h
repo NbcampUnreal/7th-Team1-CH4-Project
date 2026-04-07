b0e55b4d8a4808ea0d23328534ccd103ef96cbf3
Yoongisang <113569066+Yoongisang@users.noreply.github.com>
2026-04-07 21:34:11 +0900

    Clean up comments in BTTask_MonsterAttack.h
    Removed comments questioning the purpose of the structure and functions.

--- Source/EternalDreams/Public/AI/BTTask_MonsterAttack.h
+++ Source/EternalDreams/Public/AI/BTTask_MonsterAttack.h
@@ -5,7 +5,7 @@
 #include "CoreMinimal.h"
 #include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
 #include "BTTask_MonsterAttack.generated.h"
-// 뭘 위한 구조체? AttackMemory? 그리고 FDelegateHandle용도? MonsterBase헤더에있는 델리게이트랑 무슨 연관?
+
 struct FBTTask_AttackMemory
 {
 	FDelegateHandle OnAttackFinishedHandle;
@@ -23,9 +23,7 @@ public:
 	UBTTask_MonsterAttack();
 	
 protected:
-	// unint16? 왜? 그리고 GetInstanceMemorySize는 무슨 용도?
 	virtual uint16 GetInstanceMemorySize() const override { return sizeof(FBTTask_AttackMemory); }
-	// 각 함수 의도랑 매개변수의 풀이
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	virtual EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	virtual void OnTaskFinished(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory,EBTNodeResult::Type TaskResult) override;
