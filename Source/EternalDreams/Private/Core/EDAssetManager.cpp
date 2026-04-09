056deb071b9838befa98b975425b37d0a7be4e10
khs081215 <39721769+khs081215@users.noreply.github.com>
2026-04-09 16:00:53 +0900

    feat: Add Native GameplayTags & GameplayAbilityBase
    1) Native GameplayTags를 사용하기 위해 EDGameplayTags에서 태그를 관리합니다.
    2) GameplayAbility의 Base 클래스를 제작하였습니다.

---
+++ Source/EternalDreams/Private/Core/EDAssetManager.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/EDAssetManager.h"
+#include "Data/GameplayTag/EDGameplayTags.h"
+
+void UEDAssetManager::StartInitialLoading()
+{
+	Super::StartInitialLoading();
+	FEDGameplayTags::InitializeNativeTags();
+	//UE_LOG(LogTemp,Warning,TEXT("TagInitialized"));
+}
---
+++ Source/EternalDreams/Public/Core/EDAssetManager.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "EDAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ETERNALDREAMS_API UEDAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+	virtual void StartInitialLoading() override;	
+};
