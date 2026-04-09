dc6bab68b43b159a0ddaa9c69431abf9429f5249
khs081215 <39721769+khs081215@users.noreply.github.com>
2026-04-09 16:00:53 +0900

    feat: Add Native GameplayTags & GameplayAbilityBase
    1) Native GameplayTags를 사용하기 위해 EDGameplayTags에서 태그를 관리합니다.
    2) GameplayAbility의 Base 클래스를 제작하였습니다.

---
+++ Source/EternalDreams/Public/Characters/Player/GAS/GameplayAbility/GA_Base.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_Base.generated.h"
+
+/**
+ * GameplayAbility를 적용하는 애니메이션 몽타주 기반 행동의 부모 클래스
+ */
+UCLASS(Abstract)
+class ETERNALDREAMS_API UGA_Base : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UGA_Base();
+	
+	virtual void ActivateAbility(
+	   const FGameplayAbilitySpecHandle Handle,
+	   const FGameplayAbilityActorInfo* ActorInfo,
+	   const FGameplayAbilityActivationInfo ActivationInfo,
+	   const FGameplayEventData* TriggerEventData
+   ) override;
+	
+	virtual void EndAbility(
+	   const FGameplayAbilitySpecHandle Handle,
+	   const FGameplayAbilityActorInfo* ActorInfo,
+	   const FGameplayAbilityActivationInfo ActivationInfo,
+	   bool bReplicateEndAbility,
+	   bool bWasCancelled
+   ) override;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attack")
+	UAnimMontage* AttackMontage;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attack")
+	TSubclassOf<UGameplayEffect> DamageEffectClass;
+	
+	
+	//콜백 함수
+protected:
+	UFUNCTION()
+	void OnMontageCompleted();
+	
+	UFUNCTION()
+	void OnMontageCancelled();
+};
---
+++ Source/EternalDreams/Public/Data/GameplayTag/EDGameplayTags.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* [Native GameplayTag Guide]
+ * 게임플레이 태그를 싱글톤 클래스로 관리합니다.
+ * 
+ *  사용 예시
+ *  #include "Data/GameplayTag/EDGameplayTags.h"
+ *  FEDGameplayTags::Get().Ability_Player_BasicAttack
+ *   
+ * 기존 방식 
+ * ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Player.BasicAttack"));
+ * 변경 코드
+ * ActivationBlockedTags.AddTag(FEDGameplayTags::Get().Ability_Player_BasicAttack);
+ */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+struct ETERNALDREAMS_API FEDGameplayTags
+{
+public:
+	// Singleton 접근자
+	static const FEDGameplayTags& Get();
+
+	// 초기화
+	static void InitializeNativeTags();
+	//Ability_Monster
+	FGameplayTag Ability_Monster_Attack;
+	FGameplayTag Ability_Monster_Skill;
+	//Ability_Player
+	FGameplayTag Ability_Player_BasicAttack;
+	FGameplayTag Ability_Player_Skill;
+	FGameplayTag Ability_Player_Buff;
+	//Effect_Debuff
+	FGameplayTag Effect_Debuff_Slow;
+	FGameplayTag Effect_Debuff_Stun;
+	FGameplayTag Effect_Debuff_Poison;
+	//Effect_Buff
+	FGameplayTag Effect_Buff_AttackUp;
+	FGameplayTag Effect_Buff_Shield;
+	//State
+	FGameplayTag State_Player_CannotCanceled;
+	FGameplayTag State_Player_Invincible;
+	FGameplayTag State_Player_Unstoppable;
+;
+	//Combat
+	FGameplayTag Combat_Window_Combo;
+	FGameplayTag Combat_Window_Parry;
+	//Data
+	FGameplayTag Data_Damage;
+	FGameplayTag Data_StaminaCost;
+
+private:
+	// Singleton 인스턴스
+	static FEDGameplayTags Tags;
+};
