da14635b8a5d1e212a192c0d352a7111c22384ab
khs081215 <39721769+khs081215@users.noreply.github.com>
2026-04-13 15:54:41 +0900

    feat: Add Skill Component, Attack Logic(AnimNotifyState)
    1) 각 스킬의 GA 태그를 담고, 인풋시 콜백으로 GA를 실행하는 SkillComponent를 제작하였습니다.
    2) Anim Notify State로 공격 판정을 구현하였습니다.
    3) Player Attribute Set에 힘, 민첩, 지능을 추가하였습니다.

---
+++ Source/EternalDreams/Public/Characters/Player/Component/SkillComponent.h
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Components/ActorComponent.h"
+#include "SkillComponent.generated.h"
+
+
+class UAbilitySystemComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ETERNALDREAMS_API USkillComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	USkillComponent();
+	
+	virtual void BeginPlay() override;
+
+protected:
+	//Skill Tags
+	UPROPERTY(EditAnyWhere)
+	FGameplayTag BasicAttackTag=FGameplayTag::EmptyTag;
+	UPROPERTY(EditAnyWhere)
+	FGameplayTag QSkillTag=FGameplayTag::EmptyTag;
+	UPROPERTY(EditAnyWhere)
+	FGameplayTag ESkillTag=FGameplayTag::EmptyTag;
+	UPROPERTY(EditAnyWhere)
+	FGameplayTag SpaceSkillTag=FGameplayTag::EmptyTag;
+	
+	//Caching
+protected:
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent=nullptr;
+
+	
+#pragma region Getter/Setter
+public:
+	UFUNCTION()
+	FORCEINLINE void SetBasicAttackTag(FGameplayTag& Tag) {BasicAttackTag=Tag;};
+	UFUNCTION()
+	FORCEINLINE FGameplayTag& GetBasicAttackTag() {return BasicAttackTag;};
+
+	UFUNCTION()
+	FORCEINLINE void SetQSkillTag(FGameplayTag& Tag) {QSkillTag=Tag;};
+	UFUNCTION()
+	FORCEINLINE FGameplayTag& GetQSkillTag() {return QSkillTag;}
+	
+	UFUNCTION()
+	FORCEINLINE void SetESkillTag(FGameplayTag& Tag) {ESkillTag=Tag;};
+	UFUNCTION()
+	FORCEINLINE FGameplayTag& GetSetESkillTag() {return ESkillTag;};
+	
+	UFUNCTION()
+	FORCEINLINE void SetSpaceSkillTag(FGameplayTag& Tag) {SpaceSkillTag=Tag;};
+	UFUNCTION()
+	FORCEINLINE FGameplayTag& GetSpaceSkillTag() {return SpaceSkillTag;};
+#pragma endregion
+
+	//Activate Skill
+public:
+	UFUNCTION()
+	void ActivateBasicAttack();
+	UFUNCTION()
+	void ActivateQSkill();
+	UFUNCTION()
+	void ActivateESkill();
+	UFUNCTION()
+	void ActivateSpaceSkill();
+	
+
+};
