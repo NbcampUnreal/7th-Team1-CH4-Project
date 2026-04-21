40e9abc04883fdd7ed4835b1d74bfbc8cfd15c65
khs081215 <39721769+khs081215@users.noreply.github.com>
2026-04-21 14:05:54 +0900

    feat: Add Overlay Mat
    1) PlayerState에서 관리하는 TeamId가 다르면, 빨간색, 같으면 파란색 오버레이 머티리얼을 적용하여 피아식별이 가능하도록 하였습니다.

--- Source/EternalDreams/Private/Characters/Player/EDPlayerCharacter.cpp
+++ Source/EternalDreams/Private/Characters/Player/EDPlayerCharacter.cpp
@@ -19,10 +19,12 @@
 #include "Components/WidgetComponent.h"
 #include "Core/EDAssetManager.h"
 #include "Core/EDGameDataSubsystem.h"
+#include "Core/EDPlayerState.h"
 #include "Data/EDPlayerAnimDataAsset.h"
 #include "Data/EDPlayerDataAsset.h"
 #include "Data/EDWeaponDataAsset.h"
 #include "Data/EDPlayerDataAsset.h"
+#include "Data/EDSkillDeveloperSettings.h"
 #include "Data/GameplayTag/EDGameplayTags.h"
 #include "Data/Types/EDPlayerTypes.h"
 #include "Engine/AssetManager.h"
@@ -63,9 +65,12 @@ AEDPlayerCharacter::AEDPlayerCharacter()
 void AEDPlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-
-	//AbilitySystem 초기화
-	InitializeAbilitySystem();
+	
+	//서버에서 AbilitySystem 초기화
+	if (HasAuthority())
+	{
+		InitializeAbilitySystem();
+	}
 	
 	//서버에서만, ASC가 있는 경우 실행
 	if (HasAuthority()&&IsValid(AbilitySystemComponent))
@@ -101,6 +106,15 @@ void AEDPlayerCharacter::BeginPlay()
 	{
 		DataSubsystem->OnAllDataLoaded.AddDynamic(this, &AEDPlayerCharacter::ApplyPlayerDataAsset);
 	}
+	//Inventory Binding
+	if (IsValid(InventoryComponent))
+	{
+		InventoryComponent->OnInventoryChanged.AddDynamic(this,&AEDPlayerCharacter::OnWeaponChanged);
+		InventoryComponent->OnFirstSkillSlotChanged.AddDynamic(this,&AEDPlayerCharacter::OnFirstSkillChanged);
+		InventoryComponent->OnSecondSkillSlotChanged.AddDynamic(this,&AEDPlayerCharacter::OnSecondSkillChanged);
+	}
+	
+	
 	
 	//ASC Duration Callback
 	if (!IsValid(AbilitySystemComponent))
@@ -110,12 +124,7 @@ void AEDPlayerCharacter::BeginPlay()
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(BaseAttributeSet->GetWalkSpeedAttribute())
 	.AddUObject(this, &AEDPlayerCharacter::OnWalkSpeedChanged);
 	
-	if (IsValid(InventoryComponent))
-	{
-		InventoryComponent->OnInventoryChanged.AddDynamic(this,&AEDPlayerCharacter::OnWeaponChanged);
-		InventoryComponent->OnFirstSkillSlotChanged.AddDynamic(this,&AEDPlayerCharacter::OnFirstSkillChanged);
-		InventoryComponent->OnSecondSkillSlotChanged.AddDynamic(this,&AEDPlayerCharacter::OnSecondSkillChanged);
-	}
+	
 	
 	
 }
@@ -143,6 +152,51 @@ void AEDPlayerCharacter::Tick(float DeltaSeconds)
 	}
 	MoveVector*=DashSpeed*DeltaSeconds;
 	AddActorWorldOffset(MoveVector, true, &Hit,ETeleportType::None);
+}
+
+void AEDPlayerCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+	//AbilitySystem 초기화
+	InitializeAbilitySystem();
+	UE_LOG(LogTemp,Warning,TEXT("OnRep_PlayerState"));
+	APlayerController* LocalPC = GetWorld()->GetFirstPlayerController();
+	if (LocalPC && LocalPC->PlayerState && GetPlayerState())
+	{
+		
+		UE_LOG(LogTemp,Warning,TEXT("OnRep_PlayerState : Character"));
+		if (!IsLocallyControlled())
+		{
+			UE_LOG(LogTemp,Warning,TEXT("OnRep_PlayerState : OtherCharacter"));
+			AEDPlayerState* ThisPS=Cast<AEDPlayerState> (GetPlayerState());
+			AEDPlayerState* LocalPS=Cast<AEDPlayerState> (LocalPC->PlayerState);	
+			
+			if (ThisPS&&LocalPS)
+			{
+				UE_LOG(LogTemp,Warning,TEXT("%d %d"),ThisPS->TeamId,LocalPS->TeamId);
+				if (GetMesh()&&GetMesh()->GetChildComponent(0))
+				{
+					USkeletalMeshComponent* RetargetMeshComp=Cast<USkeletalMeshComponent>(GetMesh()->GetChildComponent(0));
+					const UEDSkillDeveloperSettings* SkillSettings = GetDefault<UEDSkillDeveloperSettings>();
+					if (RetargetMeshComp&&SkillSettings)
+					{
+						//적군 오버레이 머티리얼 설정
+						if (ThisPS->TeamId!=LocalPS->TeamId)
+						{
+							RetargetMeshComp->SetOverlayMaterial(SkillSettings->EnemyOverlayMat.LoadSynchronous());
+						}
+						//팀 오버레이 머티리얼 설정
+						else
+						{
+							RetargetMeshComp->SetOverlayMaterial(SkillSettings->TeamOverlayMat.LoadSynchronous());
+						}
+					}
+				}
+			}
+			
+		}
+	}
+	
 }
 
 
